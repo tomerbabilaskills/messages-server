b6dd0a2cedd68f029ff1dde49382a23e90bf15ce
Tomer Babila <tomerb@academy-skills.com>
2021-07-19 12:45:03 +0300

    Initialize server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const chatroomRouter = require('./chatroomRouter');
+
+const app = express();
+
+app.use(express.json());
+app.use('/chatroom', chatroomRouter);
+
+module.exports = app;
